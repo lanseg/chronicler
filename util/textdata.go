10b8c064480262b90f6cc8d2f8617488dd199db1
Your Name <you@example.com>
2022-10-14 10:02:04 +0200

    Add an overall link finder regex

--- util/textdata.go
+++ util/textdata.go
@@ -4,10 +4,26 @@ import (
 	"regexp"
 )
 
+const (
+	webLinkRegexp = "[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)"
+	ytLinkRegexp  = "((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)?"
+	ytLinkExact   = "^" + ytLinkRegexp + "$"
+)
+
 var (
-	ytLink = regexp.MustCompile("^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)?$")
+	ytLink      = regexp.MustCompile(ytLinkExact)
+	ytLinkFind  = regexp.MustCompile(ytLinkRegexp)
+	webLinkFind = regexp.MustCompile(webLinkRegexp)
 )
 
 func IsYoutubeLink(link string) bool {
 	return ytLink.Match([]byte(link))
+}
+
+func FindYoutubeLinks(text string) []string {
+	return ytLinkFind.FindAllString(text, -1)
+}
+
+func FindWebLinks(text string) []string {
+	return webLinkFind.FindAllString(text, -1)
 }
--- util/textdata_test.go
+++ util/textdata_test.go
@@ -1,7 +1,19 @@
 package util
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
+)
+
+const (
+	ytLinkFull   = "www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+	ytLinkShort  = "youtu.be/dQw4w9WgXcQ"
+	ytLinkMobile = "m.youtu.be/watch?v=dQw4w9WgXcQ"
+	ytLinkFake   = "www.yutube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+
+	tgLinkShort     = "t.me/tglinkshort"
+	twitterLinkPost = "twitter.com/Some_user/status/1234567890123456789"
 )
 
 func TestLinkCheckers(t *testing.T) {
@@ -15,25 +27,25 @@ func TestLinkCheckers(t *testing.T) {
 		{
 			desc:    "youtube regular checker success",
 			checker: IsYoutubeLink,
-			link:    "www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+			link:    ytLinkFull,
 			want:    true,
 		},
 		{
 			desc:    "youtube short checker success",
 			checker: IsYoutubeLink,
-			link:    "youtu.be/dQw4w9WgXcQ",
+			link:    ytLinkShort,
 			want:    true,
 		},
 		{
 			desc:    "youtube mobile checker success",
 			checker: IsYoutubeLink,
-			link:    "m.youtu.be/watch?v=dQw4w9WgXcQ",
+			link:    ytLinkMobile,
 			want:    true,
 		},
 		{
 			desc:    "fake youtube not matched",
 			checker: IsYoutubeLink,
-			link:    "www.yutube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+			link:    ytLinkFake,
 			want:    false,
 		},
 	} {
@@ -49,6 +61,69 @@ func TestLinkCheckers(t *testing.T) {
 			if result != tc.want {
 				t.Errorf("%v(\"%v\") expected to be %v, but got %v",
 					tc.checker, tc.link, tc.want, result)
+			}
+		})
+	}
+}
+
+func TestLinkFinders(t *testing.T) {
+	for _, tc := range []struct {
+		desc   string
+		finder func(string) []string
+		text   string
+		want   []string
+	}{
+		{
+			desc:   "youtube regular finder success",
+			finder: FindYoutubeLinks,
+			text:   ytLinkFull,
+			want:   []string{ytLinkFull},
+		},
+		{
+			desc:   "youtube short finder success",
+			finder: FindYoutubeLinks,
+			text:   ytLinkShort,
+			want:   []string{ytLinkShort},
+		},
+		{
+			desc:   "youtube mobile finder success",
+			finder: FindYoutubeLinks,
+			text:   ytLinkMobile,
+			want:   []string{ytLinkMobile},
+		},
+		{
+			desc:   "fake youtube not matched",
+			finder: FindYoutubeLinks,
+			text:   ytLinkFake,
+			want:   []string{},
+		},
+		{
+			desc:   "multiple youtube links",
+			finder: FindYoutubeLinks,
+			text: fmt.Sprintf("Lorem %s ipsum dolor %s %s sit amet, consectetur adipiscing elit, sed do eiusmod tempor %s incididunt ut labore et dolore magna aliqua.",
+				ytLinkFull, ytLinkFake, ytLinkMobile, ytLinkShort),
+			want: []string{ytLinkFull, ytLinkMobile, ytLinkShort},
+		},
+		{
+			desc:   "multiple youtube links web finder",
+			finder: FindWebLinks,
+			text: fmt.Sprintf("Lorem %s ipsum dolor %s %s sit amet, consectetur adipiscing elit, sed do eiusmod tempor %s incididunt ut labore et dolore magna aliqua.",
+				ytLinkFull, ytLinkFake, ytLinkMobile, ytLinkShort),
+			want: []string{ytLinkFull, ytLinkFake, ytLinkMobile, ytLinkShort},
+		},
+		{
+			desc:   "multiple links web finder",
+			finder: FindWebLinks,
+			text: fmt.Sprintf("Lorem %s ipsum dolor %s %s sit amet, consectetur adipiscing elit, sed do eiusmod tempor %s incididunt ut labore et dolore magna aliqua.",
+				ytLinkFull, tgLinkShort, ytLinkMobile, twitterLinkPost),
+			want: []string{ytLinkFull, tgLinkShort, ytLinkMobile, twitterLinkPost},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			result := tc.finder(tc.text)
+			if (len(result) != 0 || len(tc.want) != 0) && !reflect.DeepEqual(result, tc.want) {
+				t.Errorf("%v(\"%v\") expected to be %v, but got %v",
+					tc.finder, tc.text, tc.want, result)
 			}
 		})
 	}
