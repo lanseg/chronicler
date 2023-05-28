4c60f3631e24dbacec8a7a645758da7d95b7a659
Your Name <you@example.com>
2023-05-28 10:20:54 +0200

    Some style improvements for the thread reader

--- frontend/main.go
+++ frontend/main.go
@@ -11,7 +11,7 @@ import (
 var (
 	port        = flag.Int("port", 8080, "Port for the http server")
 	storageRoot = flag.String("storage_root", "chronicler_storage", "A local folder to save downloads.")
-	staticRoot  = flag.String("static_root", "./static", "Root directory with the web page files. ")
+	staticRoot  = flag.String("static_root", "frontend/static", "Root directory with the web page files. ")
 
 	log = util.NewLogger("main")
 )
@@ -23,7 +23,7 @@ func main() {
 	cwd, _ := os.Getwd()
 	logger.Infof("Currect directory: %s", cwd)
 	logger.Infof("Storage root: %s", *storageRoot)
-	logger.Infof("Static files root: %s", *storageRoot)
+	logger.Infof("Static files root: %s", *staticRoot)
 	logger.Infof("Starting server on port %d", *port)
 
 	server := frontend.NewServer(*port, *storageRoot, *staticRoot)
