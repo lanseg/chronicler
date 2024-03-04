418b988cf20562bea74ec7b713ff7d20a1081128
lans <>
2024-03-04 18:49:51 +0000

    Added code for getters

--- storage/endpoint/client.go
+++ storage/endpoint/client.go
@@ -1,7 +1,9 @@
 package endpoint
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"io"
 
 	"google.golang.org/grpc"
@@ -54,19 +56,54 @@ func (rs *remoteStorage) SaveRecordSet(r *rpb.RecordSet) error {
 }
 
 func (rs *remoteStorage) ListRecordSets() optional.Optional[[]*rpb.RecordSet] {
-	return optional.Of([]*rpb.RecordSet{})
+	recv, err := rs.client.List(rs.context, &ep.ListRequest{})
+	if err != nil {
+		return optional.OfError[[]*rpb.RecordSet](nil, err)
+	}
+	result := []*rpb.RecordSet{}
+	for {
+		rs, err := recv.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return optional.OfError[[]*rpb.RecordSet](nil, err)
+		}
+		result = append(result, rs.RecordSet)
+	}
+	return optional.Of(result)
 }
 
 func (rs *remoteStorage) GetRecordSet(id string) optional.Optional[*rpb.RecordSet] {
-	return optional.Of[*rpb.RecordSet](nil)
+	return optional.MapErr(optional.OfError(rs.client.Get(rs.context, &ep.GetRequest{
+		RecordSetIds: []string{id},
+	})), func(resp *ep.GetResponse) (*rpb.RecordSet, error) {
+		rss := resp.RecordSets
+		if len(rss) != 1 {
+			return nil, fmt.Errorf("Expected to get 1 record set, but got %d", len(rss))
+		}
+		return rss[0], nil
+	})
 }
 
 func (rs *remoteStorage) DeleteRecordSet(id string) error {
-	return nil
+	_, err := rs.client.Delete(rs.context, &ep.DeleteRequest{RecordSetIds: []string{id}})
+	return err
 }
 
 func (rs *remoteStorage) GetFile(id string, filename string) optional.Optional[io.ReadCloser] {
-	return optional.Of[io.ReadCloser](nil)
+	files, err := ReadAll(rs.client.GetFile(rs.context, &ep.GetFileRequest{
+		File: []*ep.GetFileRequest_FileDef{
+			{RecordSetId: id, Filename: filename},
+		},
+	}))
+	if err != nil {
+		return optional.OfError[io.ReadCloser](nil, err)
+	}
+	if files == nil || len(files) == 0 || files[0] == nil {
+		return optional.OfError[io.ReadCloser](nil, fmt.Errorf("File %s/%s not found", id, filename))
+	}
+	return optional.Of[io.ReadCloser](io.NopCloser(bytes.NewReader(files[0].Data)))
 }
 
 func (rs *remoteStorage) PutFile(id string, filename string, src io.Reader) error {
