a5ec6f9dff92dc2a900e4ed436047fd3cad2c126
Your Name <you@example.com>
2024-04-11 15:50:10 +0200

    Added query param to the controls

--- frontend/static/modules/api.js
+++ frontend/static/modules/api.js
@@ -1,19 +1,19 @@
 "use strict";
 
-export async function getRecordSets(offset = 0, size = 10) {
-    return fetch(`/chronicler/records?offset=${offset}&size=${size}`)
+export async function getRecordSets(offset = 0, size = 10, query = "") {
+    return fetch(`/chronicler/records?offset=${encodeURI(offset)}&size=${encodeURI(size)}&query=${encodeURI(query)}`)
         .then((response) => response.text())
         .then((text) => JSON.parse(text));
 }
 
 export async function getRecord(id) {
-    return fetch(`/chronicler/records/${id}`)
+    return fetch(`/chronicler/records/${encodeURI(id)}`)
         .then((response) => response.text())
         .then((text) => JSON.parse(text));
 }
 
 export async function deleteRecordSets(ids) {
-    return fetch(`chronicler/records/delete?ids=${ids.join(",")}`)
+    return fetch(`chronicler/records/delete?ids=${encodeURI(ids.join(","))}`)
         .then((response) => response.text())
         .then((text) => JSON.parse(text));
 }
