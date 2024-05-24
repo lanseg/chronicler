f89537254e9e7a2b97d32b9acb33f0c70f04fa75
Your Name <you@example.com>
2024-05-24 10:37:29 +0200

    Using synchronized containers for the cache

--- storage/localstorage.go
+++ storage/localstorage.go
@@ -9,7 +9,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/lanseg/golang-commons/collections"
+	aio "github.com/lanseg/golang-commons/almostio"
+	col "github.com/lanseg/golang-commons/collections"
 	cm "github.com/lanseg/golang-commons/common"
 	opt "github.com/lanseg/golang-commons/optional"
 
@@ -20,17 +21,24 @@ import (
 type localStorage struct {
 	Storage
 
-	mux         sync.Mutex
-	overlay     *Overlay
-	logger      *cm.Logger
-	root        string
-	recordCache map[string]*rpb.RecordSet
+	ovlmux  sync.Mutex
+	ovlid   string
+	overlay aio.Overlay
+
+	recordCache *sync.Map
+
+	logger *cm.Logger
+	root   string
 }
 
-func (s *localStorage) getOverlay(id string) *Overlay {
-	overlayRoot := filepath.Join(s.root, id)
-	if s.overlay == nil || s.overlay.root != overlayRoot {
-		s.overlay = NewOverlay(overlayRoot, cm.UUID4)
+func (s *localStorage) getOverlay(id string) aio.Overlay {
+	s.ovlmux.Lock()
+	defer s.ovlmux.Unlock()
+	if s.overlay == nil || s.ovlid != id {
+		ovl, _ := aio.NewLocalOverlay(
+			filepath.Join(s.root, id),
+			aio.NewJsonMarshal[aio.OverlayMetadata]())
+		s.overlay = ovl
 	}
 	return s.overlay
 }
@@ -46,14 +54,18 @@ func (s *localStorage) GetRecordSet(id string) opt.Optional[*rpb.RecordSet] {
 
 func (s *localStorage) GetFile(id string, filename string) opt.Optional[io.ReadCloser] {
 	s.logger.Infof("GetFile %s %s", id, filename)
-	return s.getOverlay(id).Read(filename)
+	return opt.OfError(s.getOverlay(id).OpenRead(filename))
 }
 
 func (s *localStorage) PutFile(id string, filename string, src io.Reader) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
 	s.logger.Infof("PutFile %s/%s", id, filename)
-	return s.getOverlay(id).CopyFrom(filename, src)
+	wc, err := s.getOverlay(id).OpenWrite(filename)
+	defer wc.Close()
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(wc, src)
+	return err
 }
 
 func (s *localStorage) ListRecordSets(request *rpb.ListRecordsRequest) opt.Optional[[]*rpb.RecordSet] {
@@ -68,15 +80,12 @@ func (s *localStorage) ListRecordSets(request *rpb.ListRecordsRequest) opt.Optio
 		}
 	}
 	values := []*rpb.RecordSet{}
-	if request.Query == "" {
-		values = collections.Values(s.recordCache)
-	} else {
-		for _, rs := range collections.Values(s.recordCache) {
-			if strings.Contains(fmt.Sprintf("%s", rs), request.Query) {
-				values = append(values, rs)
-			}
+	s.recordCache.Range(func(key, value any) bool {
+		if request.Query == "" || strings.Contains(fmt.Sprintf("%s", value), request.Query) {
+			values = append(values, value.(*rpb.RecordSet))
 		}
-	}
+		return true
+	})
 	sorted := records.SortRecordSets(values, sorting)
 	return opt.Of(sorted[min(len(sorted), int(paging.Offset)):min(len(sorted), int(paging.Offset+paging.Size))])
 }
@@ -85,20 +94,18 @@ func (s *localStorage) DeleteRecordSet(id string) error {
 	if len(id) != 36 {
 		return fmt.Errorf("Looks like uuid is incorrect: %q", id)
 	}
-	s.mux.Lock()
-	defer s.mux.Unlock()
+	s.ovlmux.Lock()
+	defer s.ovlmux.Unlock()
 	path := filepath.Join(s.root, id)
 	if err := os.RemoveAll(filepath.Join(s.root, id)); err != nil {
 		return err
 	}
-	delete(s.recordCache, id)
+	s.recordCache.Delete(id)
 	s.logger.Debugf("Deleted recordset at %s", path)
 	return nil
 }
 
 func (s *localStorage) SaveRecordSet(newSet *rpb.RecordSet) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
 	if newSet.Id == "" {
 		return fmt.Errorf("Record without an id")
 	}
@@ -128,25 +135,26 @@ func (s *localStorage) writeRecordSet(rs *rpb.RecordSet) error {
 		return err
 	}
 
-	_, err = s.getOverlay(rs.Id).Write(recordsetFileName, bytes).Get()
+	wc, err := s.getOverlay(rs.Id).OpenWrite(recordsetFileName)
 	if err != nil {
 		return err
 	}
+	wc.Write(bytes)
 
-	s.recordCache[rs.Id] = rs
+	s.recordCache.Store(rs.Id, rs)
 	s.logger.Infof("Saved new record to %s", rs.Id)
-	return nil
+	return wc.Close()
 }
 
 func (s *localStorage) refreshCache() {
 	s.logger.Infof("Refreshing cache")
-	s.recordCache = map[string]*rpb.RecordSet{}
+	recordCache := map[string]*rpb.RecordSet{}
 	s.getAllRecords().IfPresent(func(allRecords []*rpb.RecordSet) {
 		s.logger.Infof("Found %d records", len(allRecords))
-		for k, v := range collections.GroupBy(allRecords, func(rs *rpb.RecordSet) string {
+		for k, v := range col.GroupBy(allRecords, func(rs *rpb.RecordSet) string {
 			return rs.Id
 		}) {
-			s.recordCache[k] = v[0]
+			recordCache[k] = v[0]
 		}
 	})
 
@@ -157,8 +165,9 @@ func NewLocalStorage(root string) Storage {
 	log.Infof("Storage root set to \"%s\"", root)
 
 	ls := &localStorage{
-		root:   root,
-		logger: log,
+		root:        root,
+		logger:      log,
+		recordCache: &sync.Map{},
 	}
 	ls.refreshCache()
 	return ls
