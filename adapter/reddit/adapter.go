8f6d65d61d8dfa0ba10cd21b02e41f5dead18f87
Andrey Rusakov <andrey.rusakov@camptocamp.com>
2025-02-14 17:39:42 +0100

    Added basic Reddit parser and adapter

--- adapter/reddit/adapter.go
+++ adapter/reddit/adapter.go
@@ -1,38 +1,97 @@
 package reddit
 
 import (
+	"fmt"
+	"mime"
+	"net/url"
+	"path/filepath"
+
 	"chronicler/adapter"
 	"chronicler/common"
 	opb "chronicler/proto"
-	"regexp"
-)
-
-// (\.|^|//)(twitter|x.com).*/(?P<twitter_id>[0-9]+)[/]?`
-// https://www.reddit.com/r/law/comments/1inaszr/comment/mc9tny7/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-var (
-	redditRe = regexp.MustCompile(`reddit.com/r/(?P<subreddit>[^/]*)/comments/(?P<author>[^/]*)/(?P<maybePostName>[^/$]*)(?:/(?P<maybeCommentId>[^/$]*))?`)
 )
 
 type redditAdapter struct {
 	adapter.Adapter
 
 	logger *common.Logger
-	client adapter.HttpClient
+	client Client
+}
+
+func getMime(href string) string {
+	fileName := ""
+	if u, err := url.Parse(href); err == nil {
+		fileName = u.Path
+	} else {
+		fileName = href
+	}
+	return mime.TypeByExtension(filepath.Ext(fileName))
 }
 
 func NewAdapter(client adapter.HttpClient) adapter.Adapter {
 	return &redditAdapter{
 		logger: common.NewLogger("TwitterAdapter"),
-		client: client,
+		client: NewAnonymousClient(client),
 	}
 }
 
 func (ta *redditAdapter) Match(link *opb.Link) bool {
 	postDef := ParseLink(link.Href)
-	return postDef.Author != "" && postDef.MaybePost != "" && postDef.Subreddit != ""
+	return postDef.PostId != "" && postDef.Subreddit != ""
 }
 
 func (ta *redditAdapter) Get(link *opb.Link) ([]*opb.Object, error) {
+	postDef := ParseLink(link.Href)
+	if postDef.Subreddit == "" || postDef.PostId == "" {
+		return nil, fmt.Errorf("%s is not a Reddit post link", link.Href)
+	}
+	entities, err := ta.client.GetPost(postDef)
+	if err != nil {
+		return nil, err
+	}
 	result := []*opb.Object{}
+	for _, e := range entities {
+		attachments := []*opb.Attachment{}
+		if e.Media != nil && e.Media.RedditVideo != nil {
+			attachments = append(attachments, &opb.Attachment{
+				Url:  e.Media.RedditVideo.FallbackUrl,
+				Mime: getMime(e.Media.RedditVideo.FallbackUrl),
+			})
+		}
+		if e.Preview != nil {
+			for _, img := range e.Preview.Images {
+				attachments = append(attachments, &opb.Attachment{
+					Url:  img.Source.Url,
+					Mime: getMime(img.Source.Url),
+				})
+			}
+		}
+		for _, v := range e.MediaMetadata {
+			attachments = append(attachments, &opb.Attachment{
+				Url:  v.Original.Url,
+				Mime: v.MimeType,
+			})
+		}
+		content := []*opb.Content{}
+		if e.BodyHtml != "" {
+			content = append(content, &opb.Content{Text: e.BodyHtml, Mime: "text/html"})
+		} else if e.Body != "" {
+			content = append(content, &opb.Content{Text: e.BodyHtml, Mime: "text/plain"})
+		}
+		result = append(result, &opb.Object{
+			Id:     e.Id,
+			Parent: e.ParentId,
+			CreatedAt: &opb.Timestamp{
+				Seconds: int64(e.CreatedUtc),
+			},
+			Stats: []*opb.Stats{
+				{Type: opb.Stats_UPVOTE, Counter: int64(e.Ups)},
+				{Type: opb.Stats_DOWNVOTE, Counter: int64(e.Downs)},
+			},
+			Attachment: attachments,
+			Generator:  []*opb.Generator{{Id: e.AuthotFullName, Name: e.Author}},
+			Content:    content,
+		})
+	}
 	return result, nil
 }
--- adapter/reddit/client.go
+++ adapter/reddit/client.go
@@ -1,142 +1,32 @@
 package reddit
 
 import (
+	"chronicler/adapter"
+	"chronicler/common"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
-	"strconv"
+	"regexp"
+	"sort"
+)
+
+var (
+	redditRe = regexp.MustCompile(`reddit.com/r/(?P<subreddit>[^/]*)/comments/(?P<postId>[^/]*)/?(?P<maybePostName>[^/$]*)(?:/(?P<maybeCommentId>[^/$]*))?`)
 )
 
 type RedditPostDef struct {
 	Subreddit      string
-	Author         string
+	PostId         string
 	MaybePost      string
 	MaybeCommentId string
 }
 
-type Thing[T any] struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-	Kind string `json:"kind"`
-	Data T      `json:"data"`
-}
-
-type Listing[T any] struct {
-	Before   string `json:"before"`
-	After    string `json:"after"`
-	Modhash  string `json:"modhash"`
-	Children []T    `json:"children"`
-}
-
-type Replies Thing[Listing[Thing[Comment]]]
-
-type RepliesWrapper struct {
-	Replies *Replies
-	Empty   bool
-}
-
-func (cr *RepliesWrapper) UnmarshalJSON(data []byte) error {
-	if len(data) == 2 {
-		cr.Empty = true
-		return nil
-	}
-	cr.Replies = &Replies{}
-	return json.Unmarshal(data, cr.Replies)
-}
-
-type FalseOrFloat float64
-
-func (cr *FalseOrFloat) UnmarshalJSON(data []byte) error {
-	result := FalseOrFloat(100500.0)
-	if data[0] == 'f' || data[0] == 'F' {
-		*cr = result
-		return nil
-	}
-	asFloat, err := strconv.ParseFloat(string(data), 64)
-	if err != nil {
-		return err
-	}
-	*cr = FalseOrFloat(asFloat)
-	return nil
-}
-
-type Comment struct {
-	// The number of upvotes
-	Ups int32 `json:"Ups"`
-
-	// The number of downvotes
-	Downs int32 `json:"Downs"`
-
-	// the time of creation in UTC epoch-second format. Note that neither of these ever have a non-zero fraction
-	CreatedUTC float32 `json:"created_utc"`
-
-	//who approved this comment. null if nobody or you are not a mod
-	Approved_by string `json:"approved_by"`
-
-	//the account name of the poster
-	Author string `json:"author"`
-
-	//the CSS class of the author's flair. subreddit specific
-	AuthorFlairCssClass string `json:"author_flair_css_class"`
-
-	//the text of the author's flair. subreddit specific
-	AuthorFlairText string `json:"author_flair_text"`
-
-	//who removed this comment. null if nobody or you are not a mod
-	BannedBy string `json:"banned_by"`
-
-	//the raw text. this is the unformatted text which includes the raw markup characters such as ** for bold. <, >, and & are escaped.
-	Body string `json:"body"`
-
-	//the formatted HTML text as displayed on reddit. For example, text that is emphasised by * will now have <em> tags wrapping it. Additionally, bullets and numbered lists will now be in HTML list format. NOTE: The HTML string will be escaped. You must unescape to get the raw HTML.
-	BodyHtml string `json:"body_html"`
-
-	//false if not edited, edit date in UTC epoch-seconds otherwise. NOTE: for some old edited comments on reddit.com, this will be set to true instead of edit date.
-	Edited *FalseOrFloat `json:"edited"`
-
-	//the number of times this comment received reddit gold
-	Gilded int `json:"gilded"`
-
-	//how the logged-in user has voted on the comment - True = upvoted, False = downvoted, null = no vote
-	Likes bool `json:"likes"`
-
-	//present if the comment is being displayed outside its thread (user pages, /r/subreddit/comments/.json, etc.). Contains the author of the parent link
-	LinkAuthor string `json:"link_author"`
-
-	//ID of the link this comment is in
-	LinkId string `json:"link_id"`
-
-	//present if the comment is being displayed outside its thread (user pages, /r/subreddit/comments/.json, etc.). Contains the title of the parent link
-	LinkTitle string `json:"link_title"`
-
-	//present if the comment is being displayed outside its thread (user pages, /r/subreddit/comments/.json, etc.). Contains the URL of the parent link
-	LinkUrl string `json:"link_url"`
-
-	//how many times this comment has been reported, null if not a mod
-	NumReports int `json:"num_reports"`
-
-	//ID of the thing this comment is a reply to, either the link or a comment in it
-	ParentId string `json:"parent_id"`
-
-	//A list of replies to this comment
-	Replies *RepliesWrapper `json:"replies"`
-
-	//true if this post is saved by the logged in user
-	Saved bool `json:"saved"`
-
-	//the net-score of the comment
-	Score int `json:"score"`
-
-	//Whether the comment's score is currently hidden.
-	ScoreHidden bool `json:"score_hidden"`
-
-	//subreddit of thing excluding the /r/ prefix. "pics"
-	Subreddit string `json:"subreddit"`
-
-	//the id of the subreddit in which the thing is located
-	SubredditId string `json:"subreddit_id"`
-
-	//to allow determining whether they have been distinguished by moderators/admins. null = not distinguished. moderator = the green [M]. admin = the red [A]. special = various other special distinguishes http://redd.it/19ak1b
-	Distinguished string `json:"distinguished"`
+type GetPostResponse struct {
+	Entities []*Entity `json:"entities"`
+	More     []string  `json:"more"`
 }
 
 func ParseLink(link string) *RedditPostDef {
@@ -147,8 +37,8 @@ func ParseLink(link string) *RedditPostDef {
 		switch subexp[i] {
 		case "subreddit":
 			result.Subreddit = key
-		case "author":
-			result.Author = key
+		case "postId":
+			result.PostId = key
 		case "maybePostName":
 			result.MaybePost = key
 		case "maybeCommentId":
@@ -159,25 +49,97 @@ func ParseLink(link string) *RedditPostDef {
 }
 
 type Client interface {
-	GetPost(def *RedditPostDef) error
+	GetPost(def *RedditPostDef) ([]*Entity, error)
+}
+
+type RedditAuth struct {
+	AccessToken  string
+	RefreshToken string
 }
 
 type redditClient struct {
+	auth       *RedditAuth
+	httpClient adapter.HttpClient
+	logger     *common.Logger
 }
 
-func (rc *redditClient) GetPost(def *RedditPostDef) error {
-	result := []Thing[Listing[Thing[Comment]]]{}
-	resdata, err := os.ReadFile("/home/arusakov/devel/lanseg/chronicler/reddit.json")
+func (rc *redditClient) get(def *RedditPostDef) ([]Thing[Listing[Thing[Entity]]], error) {
+	resp, err := rc.httpClient.Do(&http.Request{
+		Method: "GET",
+		URL: &url.URL{
+			Scheme:   "https",
+			Host:     "www.reddit.com",
+			Path:     fmt.Sprintf("/r/%s/comments/%s.json", def.Subreddit, def.PostId),
+			RawQuery: "threaded=false&limit=1000000",
+		},
+		Header: http.Header{
+			"Content-type": []string{"application/x-www-form-urlencoded"},
+		},
+	})
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if err = json.Unmarshal(resdata, &result); err != nil {
-		return err
+	defer resp.Body.Close()
+
+	responseBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	os.WriteFile("/home/arusakov/devel/lanseg/chronicler/reddit.json_", responseBytes, 0777)
+	result := []Thing[Listing[Thing[Entity]]]{}
+	if err = json.Unmarshal(responseBytes, &result); err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (rc *redditClient) fetch(def *RedditPostDef) (*GetPostResponse, error) {
+	result := &GetPostResponse{}
+	redditPost, err := rc.get(def)
+	if err != nil {
+		return nil, err
+	}
+	toFetch := map[string]bool{}
+	for _, th := range redditPost {
+		for _, the := range th.Data.Children {
+			if the.Kind == "more" {
+				toFetch[the.Data.Id] = true
+				for _, ch := range the.Data.Children {
+					toFetch[ch] = true
+				}
+			} else {
+				result.Entities = append(result.Entities, &the.Data)
+			}
+		}
+		th.Data.Children = nil
 	}
 
-	resBAfter, err := json.Marshal(result)
-	if err != nil {
-		return err
+	for k := range toFetch {
+		result.More = append(result.More, k)
 	}
-	return os.WriteFile("/home/arusakov/devel/lanseg/chronicler/reddit-out.json", resBAfter, 0777)
+	sort.Strings(result.More)
+	return result, nil
+}
+
+func (rc *redditClient) GetPost(def *RedditPostDef) ([]*Entity, error) {
+	rc.logger.Infof("Loading post and comments from %s/%s", def.Subreddit, def.MaybePost)
+	post, err := rc.fetch(def)
+	if err != nil {
+		return nil, err
+	}
+	rc.logger.Infof("Loaded entities from %s/%s: %d, still to load at least %d",
+		def.Subreddit, def.MaybePost, len(post.Entities), len(post.More))
+	return post.Entities, nil
+}
+
+func NewAnonymousClient(client adapter.HttpClient) Client {
+	return NewClient(client, nil)
+}
+
+func NewClient(client adapter.HttpClient, auth *RedditAuth) Client {
+	return &redditClient{
+		auth:       auth,
+		logger:     common.NewLogger("RedditClient"),
+		httpClient: client,
+	}
 }
